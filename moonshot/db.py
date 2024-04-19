670e167f7a5fc965c7dd23fc772fbf9744c20192
DESKTOP-9OFQO9M\sandesh <sandeshgangadhar94@gmail.com>
2024-04-19 14:29:59 +0530

    code cleanup

--- moonshot/db.py
+++ moonshot/db.py
@@ -38,33 +38,3 @@ def insert_to_collections(database_name, collection_name, data):
     db = client[database_name]
     collections = db[collection_name]
     return collections.insert_many(data)
-
-
-# from sqlalchemy import create_engine, Column, Integer, String
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = 'sqlite:///moonshot.db'
-# Base = declarative_base()
-
-# class DataTable(Base):
-#     __tablename__ = 'moonshot_data'
-
-#     id = Column(Integer, primary_key=True)
-#     source = Column(String)
-#     original_text = Column(String)
-#     word_frequencies = Column(String)
-
-
-# engine = create_engine(DATABASE_URL, echo=True)
-
-# Base.metadata.create_all(engine)
-
-# def start_session():
-#     Session = sessionmaker(bind=engine)
-#     return Session()
-
-
-# def main():
-
-#    pass
